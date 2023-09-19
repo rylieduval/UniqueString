f2fb1243d7cab1768302e2db74f5f644046898e3
rylieduval <129247479+rylieduval@users.noreply.github.com>
2023-09-18 21:20:34 -0500

    Update UniqueCharacters.java

--- UniqueCharacters.java
+++ UniqueCharacters.java
@@ -11,8 +11,10 @@ public class UniqueCharacters
     
     public static boolean IsAllUnique(String str)
     {   
+        //counter to see how many letters are the same
         int counter = 0; 
-        
+
+        //goes through each letter, and compares each letter to each other
         for (int i = 0; i < str.length() - 1; i++)
         {
             counter = 0;
@@ -21,17 +23,19 @@ public class UniqueCharacters
             {
                 if (str.charAt(i) == str.charAt(j))
                 {
+                    //increases counter if the letters are the same
                     counter++;
                 }
             }
             
             if (counter > 1)
             {
+                    //returns false if it matches with another letter in the word, besides itself
                     return false;
             }
             
         }
-        
+        //returns true if it doesn't find another letter match
         return true;
     }
     
